9686a071440d4672f90f4c42c7b752ed6b696b92
ilya21 <Ilya.komendantov@list.ru>
2024-01-27 16:23:44 +0300

    Initial commit

---
+++ app/src/main/java/ru/kraz/lazycolumnrange/MainActivity.kt
@@ -0,0 +1,93 @@
+package ru.kraz.lazycolumnrange
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.kraz.lazycolumnrange.ui.theme.LazyColumnRangeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumnRangeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LazyColumnRange()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LazyColumnRange(viewModel: MainViewModel = MainViewModel()) {
+    val list by viewModel.uiState.collectAsState(initial = emptyList())
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            itemsIndexed(list) { index, item ->
+                Element(item) {
+                    viewModel.set(index)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Element(item: ItemUi, setState: () -> Unit) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { setState() }
+            .background(if (item.selected) Color.LightGray else Color.White)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = item.text,
+            Modifier
+                .fillMaxSize()
+                .background(if (item.selected) Color.LightGray else Color.White),
+            color = Color.Black
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LazyColumnRangeTheme {
+        LazyColumnRange()
+    }
+}
---
+++ app/src/main/java/ru/kraz/lazycolumnrange/MainViewModel.kt
@@ -0,0 +1,34 @@
+package ru.kraz.lazycolumnrange
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel : ViewModel() {
+
+    private val items = mutableListOf<ItemUi>()
+    private val _uiState = MutableStateFlow<List<ItemUi>>(emptyList())
+    val uiState: StateFlow<List<ItemUi>> get() = _uiState
+
+    init {
+        (0..50).map {
+            items.add(ItemUi(it, "Item $it"))
+        }
+
+        _uiState.value = items.toList()
+    }
+
+    fun set(position: Int) {
+        for (index in 0..position) {
+            items[index] = items[index].copy(selected = !items[position].selected)
+        }
+
+        _uiState.value = items.toList()
+    }
+}
+
+data class ItemUi(
+    val id: Int,
+    val text: String,
+    val selected: Boolean = false
+)
