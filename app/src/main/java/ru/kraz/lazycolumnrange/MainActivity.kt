bb0c55eddeba8f156a728ce9044d8efef560d6dc
ilya21 <Ilya.komendantov@list.ru>
2024-01-31 20:09:11 +0300

    chat

--- app/src/main/java/ru/kraz/lazycolumnrange/MainActivity.kt
+++ app/src/main/java/ru/kraz/lazycolumnrange/MainActivity.kt
@@ -3,30 +3,43 @@ package ru.kraz.lazycolumnrange
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import ru.kraz.lazycolumnrange.ui.theme.LazyColumnRangeTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,40 +60,98 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LazyColumnRange(viewModel: MainViewModel = MainViewModel()) {
-    val list by viewModel.uiState.collectAsState(initial = emptyList())
+    val messages by viewModel.uiState.collectAsState()
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
+    var textFieldValue by rememberSaveable { mutableStateOf("") }
+
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            itemsIndexed(list) { index, item ->
-                Element(item) {
-                    viewModel.set(index)
+            LazyColumn {
+                items(messages) {
+                    Message(item = it)
                 }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                TextField(modifier = Modifier.weight(1f),
+                    value = textFieldValue,
+                    onValueChange = {
+                        textFieldValue = it
+                    })
+                Image(
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
+                        .wrapContentHeight(Alignment.CenterVertically)
+                        .clickable {
+
+                            if (textFieldValue.isNotEmpty()) {
+                                viewModel.sendMessage(textFieldValue)
+                                textFieldValue = ""
+                            }
+                        },
+                    painter = painterResource(id = android.R.drawable.ic_menu_send),
+                    contentDescription = null
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Element(item: ItemUi, setState: () -> Unit) {
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { setState() }
-            .background(if (item.selected) Color.LightGray else Color.White)
-            .padding(16.dp)
-    ) {
-        Text(
-            text = item.text,
-            Modifier
-                .fillMaxSize()
-                .background(if (item.selected) Color.LightGray else Color.White),
-            color = Color.Black
-        )
+fun Message(item: MessageUi) {
+    if (item.iSendThis) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .padding(4.dp),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            Card(modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .fillMaxHeight()
+                .align(Alignment.TopEnd), onClick = {}) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(modifier = Modifier.wrapContentSize(), text = item.text)
+                    Image(
+                        modifier = Modifier
+                            .width(15.dp)
+                            .height(15.dp)
+                            .align(Alignment.End),
+                        painter = painterResource(id = if (item.messageRead) R.drawable.done_all else R.drawable.done),
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    } else {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .padding(4.dp),
+            contentAlignment = Alignment.TopStart
+        ) {
+            Card(modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .fillMaxHeight(), onClick = {}) {
+                Text(modifier = Modifier.padding(4.dp), text = item.text)
+            }
+        }
     }
 }
 
--- app/src/main/java/ru/kraz/lazycolumnrange/MainViewModel.kt
+++ app/src/main/java/ru/kraz/lazycolumnrange/MainViewModel.kt
@@ -1,34 +1,34 @@
 package ru.kraz.lazycolumnrange
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
 
-    private val items = mutableListOf<ItemUi>()
-    private val _uiState = MutableStateFlow<List<ItemUi>>(emptyList())
-    val uiState: StateFlow<List<ItemUi>> get() = _uiState
+    private val messages = mutableListOf<MessageUi>()
+    private val _uiState = MutableStateFlow<List<MessageUi>>(emptyList())
+    val uiState: StateFlow<List<MessageUi>> get() = _uiState
 
-    init {
-        (0..50).map {
-            items.add(ItemUi(it, "Item $it"))
-        }
-
-        _uiState.value = items.toList()
+    fun sendMessage(text: String) = viewModelScope.launch(Dispatchers.IO) {
+        messages.add(MessageUi(text = text, iSendThis = Random.nextBoolean()))
+        _uiState.value = messages.toList()
     }
 
-    fun set(position: Int) {
-        for (index in 0..position) {
-            items[index] = items[index].copy(selected = !items[position].selected)
-        }
-
-        _uiState.value = items.toList()
+    private fun readMessage(position: Int) {
+        messages[position] = messages[position].copy(messageRead = true)
+        _uiState.value = messages.toList()
     }
 }
 
-data class ItemUi(
-    val id: Int,
+data class MessageUi(
+    val id: Long = System.currentTimeMillis(),
     val text: String,
-    val selected: Boolean = false
+    val iSendThis: Boolean,
+    val messageRead: Boolean = false
 )
