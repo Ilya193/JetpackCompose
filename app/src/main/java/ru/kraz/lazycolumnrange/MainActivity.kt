5ef7f682230ae0d501f721603ed6b0204c85c5dc
ilya21 <Ilya.komendantov@list.ru>
2024-01-31 23:04:03 +0300

    horizontal pager

--- app/src/main/java/ru/kraz/lazycolumnrange/MainActivity.kt
+++ app/src/main/java/ru/kraz/lazycolumnrange/MainActivity.kt
@@ -1,32 +1,37 @@
 package ru.kraz.lazycolumnrange
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import ru.kraz.lazycolumnrange.ui.theme.LazyColumnRangeTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,49 +50,72 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LazyColumnRange(viewModel: MainViewModel = MainViewModel()) {
-    val list by viewModel.uiState.collectAsState(initial = emptyList())
+    val images = remember { mutableStateListOf<String>() }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            itemsIndexed(list) { index, item ->
-                Element(item) {
-                    viewModel.set(index)
-                }
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) {
+            it?.let {
+                images.add(it.toString())
             }
+        }
+
+    val pagerState = rememberPagerState { images.size }
+
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+        ) {
+            if (images.isEmpty())
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    text = "Здесь будут отображаться изображения из галереи"
+                )
+            else HorizontalPager(
+                modifier = Modifier
+                    .fillMaxSize(), state = pagerState
+            ) { index ->
+                Element(item = images[index])
+            }
+        }
+
+        Button(modifier = Modifier.fillMaxWidth(),
+            onClick = { launcher.launch("image/*") }) {
+            Text(text = "Выбрать изображение из галереи")
         }
     }
 }
 
 @Composable
-fun Element(item: ItemUi, setState: () -> Unit) {
-
-    Card(
+fun Element(item: String) {
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .clickable { setState() }
-            .background(if (item.selected) Color.LightGray else Color.White)
-            .padding(16.dp)
+            .fillMaxSize()
     ) {
-        Text(
-            text = item.text,
-            Modifier
-                .fillMaxSize()
-                .background(if (item.selected) Color.LightGray else Color.White),
-            color = Color.Black
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = item,
+            contentDescription = null,
+            contentScale = ContentScale.Crop
         )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyColumnRangeTheme {
-        LazyColumnRange()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(25.dp)
+                .background(Color.Gray)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = Uri.parse(item).lastPathSegment.toString(),
+                style = TextStyle(color = Color.Black)
+            )
+        }
     }
 }
